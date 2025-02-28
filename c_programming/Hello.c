36654c84b4672aa9c958d3ed5b4eb3edf6704cdc
harleen001 <harleen.johal31@gmail.com>
2025-02-28 10:53:17 +0530

    variable_updated

---
+++ c_programming/Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World");
+    int number = 25;
+    char star ='*';
+    int _age=22;
+    float pi =3.14;
+    
+    _age=24; //variable can be updated like this
+    return 0;
+}
