2320c13aab1647c04cc78be82021cdd1cd13e0fb
harleen001 <harleen.johal31@gmail.com>
2025-02-28 13:17:52 +0530

    Ques-1

--- c_programming/Hello.c
+++ c_programming/Hello.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    printf("Hello World\n");  // \n will print it to next line
+    printf("Hello World\n");
     int number = 25;
     char star ='*';
     int _age=22;
     float pi =3.14;
+
+    printf("Age is %d\n",_age); //integers
+    printf("Value of pie is %f\n",pi); // real numbers
+    printf("Star look like this %c\n",star); //characters
     
     _age=24; //variable can be updated like this
     return 0;
---
+++ c_programming/Q_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int side;
+    printf("Please Enter Side of Square = \n");
+    scanf("%d",&side);
+    printf("The area of Square = %d",side*side);
+    return 0;
+}
