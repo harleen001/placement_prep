6c31d5e2d9d987ffaf3a58c04f260b478503fa05
harleen001 <harleen.johal31@gmail.com>
2025-03-24 17:20:39 +0530

    reprecussion

---
+++ dsa/arrays/Intro.cpp
@@ -0,0 +1,13 @@
+//contigious memory,random access, cache friendliness
+//fixed size and dynamic arrays
+
+#include <iostream>
+using namespace std;
+int main()
+{
+//FIXED SIZE ARRAYS    
+int arr[100]; //STACK ALLOCATED MEMORY
+int arr[n]; //STACK ALLOCATED MEMORY
+int *arr=new int[n];//HEAP ALLOCATED MEMORY
+int arr[]={10,20,30};//STACK ALLOCATED MEMORY
+}
