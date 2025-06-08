55ce83da9592ec04d6166bf1d889886191a07690
harleen001 <harleen.johal31@gmail.com>
2025-06-08 12:19:38 +0530

    check

--- dsa/arrays/Intro.cpp
+++ dsa/arrays/Intro.cpp
@@ -4,10 +4,4 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-//FIXED SIZE ARRAYS    
-int arr[100]; //STACK ALLOCATED MEMORY
-int arr[n]; //STACK ALLOCATED MEMORY
-int *arr=new int[n];//HEAP ALLOCATED MEMORY
-int arr[]={10,20,30};//STACK ALLOCATED MEMORY
-}
+ 
