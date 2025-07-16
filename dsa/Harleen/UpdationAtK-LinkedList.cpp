6e9150a28f4fa750b5122ce8ac99ed783414a01c
harleen001 <harleen.johal31@gmail.com>
2025-07-16 15:44:15 +0530

    'update-k'

---
+++ dsa/Harleen/UpdationAtK-LinkedList.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int data)
+    {
+        value=data;
+        next=NULL;
+    }
+};
+void display(Node* &head)
+{
+    Node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->value<<" ";
+        temp=temp->next;
+    }
+}
+void insertatLast(Node* &head,int element)
+{
+Node* temp=head; 
+Node* newelement=new Node(element);
+while(temp->next!=NULL)
+{
+temp=temp->next;
+}
+temp->next=newelement;
+}
+
+
+void updateK(Node* &head,int k,int newvalue)
+{
+    int count=1;
+    Node* temp=head;
+    if(count<=k && temp!=NULL)
+    {
+        temp=temp->next;
+    }
+    if(temp->next==NULL)
+    {
+        return;
+    }
+    temp->next->value=newvalue;
+
+}
+int main()
+{
+    Node* n1=new Node(10);
+    insertatLast(n1,20);
+    insertatLast(n1,40);
+    insertatLast(n1,40);
+    display(n1);
+    updateK(n1,3,30);
+    display(n1);
+}
